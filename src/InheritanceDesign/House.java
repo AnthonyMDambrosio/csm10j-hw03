b5e7e8898de1d4803705e2a84cb080ed95d093c5
AnthonyMDambrosio <anthonymdambrosio@gmail.com>
2015-12-07 18:19:10 -0800

    Create House.java

---
+++ src/InheritanceDesign/House.java
@@ -0,0 +1,46 @@
+/******************************************
+* Programmer : Anthony D'Ambrosio
+* Date       : 11/10/2015
+* Purpose    : Net Worth
+* Notes      :
+******************************************/
+package InheritanceDesign;
+
+
+public class House extends Property
+{
+    public String assetName;
+    
+    public House( String name, double propertyValue, double propertyDebt )
+    {
+        super( name, propertyValue, propertyDebt );
+    }
+    
+    @Override
+    public double getAssetValue()
+    {
+        return ( getValueAmount() );
+    }
+    
+    @Override
+    public double getDebtValue()
+    {
+        return ( getDebtAmount() );
+    }
+    
+    @Override 
+    public String toString()
+    {
+        return this.getClass().getSimpleName() + ", " + name + 
+                ", Property Value: " + getValueAmount() + ", Property Debt: " +
+                getDebtAmount() + ", Net Value: " + (
+                getValueAmount() - getDebtAmount() );
+    }
+    
+    @Override 
+    public String getAssetName()
+    {
+        return assetName;
+    }
+    
+}
