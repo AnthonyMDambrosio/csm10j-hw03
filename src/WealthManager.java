a730a3a6a40f8e30c4f357cdce67650a2c2ab4ca
AnthonyMDambrosio <anthonymdambrosio@gmail.com>
2015-12-07 18:21:42 -0800

    Create WealthManager.java

---
+++ src/WealthManager.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import InheritanceDesign.*;
+
+
+public class WealthManager  
+{
+    public static void main( String [] args )
+    {   
+        WealthManagerForm wmfObj = new WealthManagerForm();
+    }
+
+}
